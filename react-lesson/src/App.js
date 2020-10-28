c1067d1888b26b17604cfa3ef5d395fc10aa6e5c
Anton <anton123098@gmail.com>
2020-10-28 22:18:53 +0300

    ls11

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://free-images.com/or/8a4e/lion_head_isolated_art.jpg"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Messagie</a>
+                </div>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+
+            <div className='content'>
+                <div>
+                    <img className='content-img' src="http://models-talent.ru/wp-content/uploads/2017/07/1500x500.jpg"/>
+                </div>
+                <div>
+                    ava+dics
+                </div>
+                <div>
+                    Mypost
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post1
+                        </div>
+                        <div>
+                            post2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
